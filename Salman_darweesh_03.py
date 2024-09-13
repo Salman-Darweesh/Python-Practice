41534d41d884ffe96c8beb2aef0bb6e3491d72e9
Salman-Darweesh <salman.darweesh@my.utsa.edu>
2024-09-12 20:12:39 -0500

    commit

--- Salman_darweesh_03.py
+++ Salman_darweesh_03.py
@@ -10,54 +10,54 @@ if choice_1 == "no":
 
     # creating a counter
 counter = 0 
-
-if counter > 3 and counter < 2 == "Correct!":
-    print("You Failed :P")
-
     # answers to the riddles
-riddle_1 = "Your left hand"
-riddle_2 = "A towel"
+riddle_1 = "your left hand"
+riddle_2 = "a towel"
 riddle_3 = "a rubber band"
-rid_ans_4 = "G"
-rid_ans_5 = "When it is ajar"
+riddle_4 = "g"
+riddle_5 = "when it is ajar"
 
     #riddles given to the user
 rid_ans_1 = input("What can you hold in your right hand, but never in your left hand? ")
-if rid_ans_1.lower() == riddle_1: #creating a coutner to add to the original value of 0
+if rid_ans_1.lower() == riddle_1: 
     print("Correct!")
-    counter +=1
+    counter +=1 #creating a coutner to add to the original value of 0
 else:
     print("You got it wrong, L")
 
 rid_ans_2 = input("What gets wet while drying? ")
-if rid_ans_2 == "A towel":
+if rid_ans_2.lower() == riddle_2:
     print("Correct!")
     counter +=1
 else:
     print("You got it wrong, L")
     
 rid_ans_3 = input("What kind of band never plays music? ")
-if rid_ans_3 == "A rubber band":
+if rid_ans_3.lower() == riddle_3:
     print("Correct!")
     counter +=1
 else:
     print("You got it wrong, L")
     
 rid_ans_4 = input("What is the end of everything? ")
-if rid_ans_4 == "G":
+if rid_ans_4.lower() == riddle_4:
     print("Correct!")
     counter +=1
 else:
     print("You got it wrong, L")
     
 rid_ans_5 = input("When is a door no longer a door? ")
-if rid_ans_5 == "When it is ajar":
+if rid_ans_5.lower() == riddle_5:
     print("Correct!")
     counter +=1
 else:
     print("You got it wrong, L")
     
-if counter < 3 and counter < 5 == "You escaped":
+if counter > 5 and counter < 3 == "You escaped":
     print(f"there were", counter, "correct answers.")
 else:
-    print("You died")
+    print("You did not make it out alive, womp womp")
+    
+    
+    
+    #creating a coutner to add to the original value of 0
