c67bf85570dd450f2ce44898ce838695add728ee
Salman-Darweesh <salman.darweesh@my.utsa.edu>
2024-09-17 13:15:29 -0500

    commit

---
+++ Salman_Darweesh_Assesment_01.py
@@ -0,0 +1,8 @@
+number_chosen = int(input("Pick a value between 0 to 100 "))  # asking for value
+
+conversion_formul = number_chosen + 25  # creating a cconversion formula 
+
+if conversion_formul >= 74: # making a statement that accepts if youve got a value over 74
+    print ("you have passed")
+else:
+    print("you have failed to meet the requirements")
