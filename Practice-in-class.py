f65735e555ce07cf118addf40cdbd2d9de2bcacb
Salman-Darweesh <salman.darweesh@my.utsa.edu>
2024-09-19 11:23:48 -0500

    commit

--- Practice-in-class.py
+++ Practice-in-class.py
@@ -1,5 +1,5 @@
-'''
-module 4 
+
+    # module 4 
 counter = 0 
 age = int(input("Enter your age: "))
 height = float(input("Enter your height in feet: "))
@@ -10,6 +10,18 @@ if age > 12 or age < 65 or choice == "awesome":
 else:
     print("You are not tall enough or old enough to ride the roller coaster.")
         # messed up the logic of it all. 
-'''
-
         
+import random
+import statistics
+import math
+import os   #operating system
+#import pandas as pd? 
+
+random_number = random.randint(0,1000)  # 0 to 100 value, inclusive so it also can be 100 and not 99 
+print (random_number)
+result = pow(2, 3) #means 2^3 and can use positives and negatives. 
+new_result = math.sqrt(4)
+print (new_result)
+
+result = pow(2, 3) #means 2^3 and can use positives and negatives. 
+
